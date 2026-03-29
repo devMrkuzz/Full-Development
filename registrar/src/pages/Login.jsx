38d632d1c1ee6677c1eed0ac7ce6151e77e40709
Mark Julius Bongalbal <send2hire.mark@gmail.com>
2026-03-30 04:25:59 +0800

    frontend

--- registrar/src/pages/Login.jsx
+++ registrar/src/pages/Login.jsx
@@ -30,6 +30,7 @@ export default function Login() {
 
   return (
     <div className="min-h-screen bg-white flex items-center justify-center">
+      
       <div className="w-full max-w-md p-8 border border-gray-200 rounded-xl shadow-sm">
 
         <div className="text-center mb-8">
